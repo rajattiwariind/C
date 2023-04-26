47c360fb422d403d75bd8d5044820dbbc29ea05c
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-26 19:48:16 +0530

    Create Temperature.cpp

---
+++ DSA_c++/Temperature.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ int main()
+ {
+     float f,celsius;
+     
+     std::cin>>celsius;
+     std::cout<<"\n celsius ="<<celsius;
+     
+     f=1.8*celsius=32.0;
+     
+     std::cout<<"fahrenheit="<<f;
+ }
