0affe0bdacc77f4f8776c58f19a57f5db2d84ca6
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-04-26 20:18:49 +0530

    Create PerimeterOfRectangle.cpp

---
+++ DSA_c++/PerimeterOfRectangle.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+int main()
+{
+    int p,q,area,perimeter;
+    
+    p=4;
+    q=6;
+    
+    area=p*q;
+    perimeter=2*(p+q);
+    
+    std::cout<<"\n area="<<area;
+    std::cout<<"\n perimeter="<<perimeter;
+}
